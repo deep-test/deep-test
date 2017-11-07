d276f2b7e486c9af93b975e564a9257a7fc03074
deep-test <andrei@nb-m-a-chertkov.local>
2017-11-07 14:38:07 +0300

    tmp

--- db_deeptensor/buildJSON.js
+++ db_deeptensor/buildJSON.js
@@ -6,7 +6,7 @@ console.log('--------------------------------------------')
 
 const path = require("path")
 const moduleAlias = require('module-alias')
-moduleAlias.addAlias('@@', path.join(path.resolve(__dirname), './../../src'))
+moduleAlias.addAlias('@@', path.join(path.resolve(__dirname), './../../../../src'))
 const hook = require('@@/es6export/es6export')
 
 const Cs = require('@@/cs/Cs.js')
@@ -15,7 +15,7 @@ const {tblsDel$, tblExcelToMySQL$, tblMySQLToJSON$} = require('@@/database/engin
 const CS = new Cs({app: 'deeptensor', debug: true, write: true})
 CS.res('Modules are loaded')
 
-const tblNames = ['plan', 'events', 'journals', 'papers', 'team', 'imgs', 'components']
+const tblNames = ['plan', 'events', 'journals', 'papers', 'team', 'imgs']
 const opts = {
     userName: 'root',
     userPassword: 'fraksos',
@@ -30,7 +30,6 @@ Promise.resolve()
     .then  (res => tblMySQLToJSON$(opts, 'papers'))
     .then  (res => tblMySQLToJSON$(opts, 'plan'))
     .then  (res => tblMySQLToJSON$(opts, 'events'))
-    .then  (res => tblMySQLToJSON$(opts, 'components'))
     .then  (res => CS.res(`Tables are converted to JSON`))
     .catch (err => {
         CS.err(`Can't convert tables to JSON`, '', '', err)
